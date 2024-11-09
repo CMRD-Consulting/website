79e36277947822d139d3349368f64d6de6e32501
Christopher Mitchell <cmitchell@crishell.co>
2024-11-08 23:51:06 -0500

    fix: tweaks

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
       class="flex items-center justify-center md:justify-start flex-col md:flex-row md:space-x-4 flex-none"
     >
       <div
-        class="rounded overflow-hidden md:w-20 md:h-20 h-24 w-24 mb-4 md:mb-0 bg-white text-zinc-900 flex flex-col items-center justify-center text-2xl"
+        class="rounded overflow-hidden h-16 w-16 mb-4 md:mb-0 bg-white text-zinc-900 flex flex-col items-center justify-center text-2xl leading-none"
       >
         <div>CM</div>
         <div>RD</div>
@@ -13,7 +13,7 @@
       <div
         class="flex flex-col items-center md:items-start space-y-2 md:space-y-0"
       >
-        <div class="font-semibold text-2xl md:text-xl"
+        <div class="font-semibold text-xl"
           >CMRD Consulting LLC</div>
         <a href="mailto:info@cmrd.dev?subject=Hello" target="_top" class="hover:underline">info@cmrd.dev</a>
         <div v-if="false"
@@ -74,8 +74,8 @@
     <div class="flex-1">
       <nuxt-page />
     </div>
-    <div class="flex justify-center text-xs">
-      <div>&copy; {{ year }} CMRD Consulting LLC</div>
+    <div class="flex justify-center text-xs font-mono">
+      &copy; {{ year }} CMRD Consulting LLC
     </div>
   </div>
 </template>
