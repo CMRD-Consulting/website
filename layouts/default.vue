c8f3b97035f2fdac8ea43a9ef5bfdc40a8e1cb20
Christopher Mitchell <cmitchell@crishell.co>
2024-11-08 23:57:12 -0500

    feat: animations

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-10 space-y-8 h-dvh flex flex-col">
     <div
-      class="flex items-center justify-center md:justify-start flex-col md:flex-row md:space-x-4 flex-none"
+      class="flex items-center justify-center md:justify-start flex-col md:flex-row md:space-x-4 flex-none animate-fade animate-once animate-ease-in"
     >
       <div
         class="rounded overflow-hidden h-16 w-16 mb-4 md:mb-0 bg-white text-zinc-900 flex flex-col items-center justify-center text-2xl leading-none"
@@ -16,65 +16,12 @@
         <div class="font-semibold text-xl"
           >CMRD Consulting LLC</div>
         <a href="mailto:info@cmrd.dev?subject=Hello" target="_top" class="hover:underline">info@cmrd.dev</a>
-        <div v-if="false"
-          class="text-xs md:space-x-2 items-center md:items-start flex flex-col md:flex-row"
-        >
-          <div
-            class="md:space-x-2 font-semibold whitespace-no-wrap justify-center md:justify-start flex"
-          >
-            <span>Packages</span>
-            <span class="invisible md:visible">/</span>
-          </div>
-          <div>
-            <a
-              target="_blank"
-              href="https://crishellco.github.io/vue-call-store/"
-              >@crishellco/vue-call-store</a
-            >
-          </div>
-          <div>
-            <a target="_blank" href="https://crishellco.github.io/vue-beam/"
-              >@crishellco/vue-beam</a
-            >
-          </div>
-          <div>
-            <a target="_blank" href="https://crishellco.github.io/vue-spruce/"
-              >@crishellco/vue-spruce</a
-            >
-          </div>
-          <div>
-            <a target="_blank" href="https://vue-hubble.crishell.co/"
-              >@crishellco/vue-hubble</a
-            >
-          </div>
-          <div>
-            <a target="_blank" href="https://crishellco.github.io/vue-coy/"
-              >@crishellco/vue-coy</a
-            >
-          </div>
-        </div>
-        <div v-if="false"
-          class="text-xs md:space-x-2  items-center md:items-start flex flex-col md:flex-row"
-        >
-          <div class="md:space-x-2 font-semibold whitespace-no-wrap">
-            <span>Links</span>
-            <span class="invisible md:visible">/</span>
-          </div>
-          <div>
-            <a target="_blank" href="http://github.com/crishellco">GitHub</a>
-          </div>
-          <div>
-            <a target="_blank" href="https://www.linkedin.com/in/crishellco/"
-              >LinkedIn</a
-            >
-          </div>
-        </div>
       </div>
     </div>
     <div class="flex-1">
       <nuxt-page />
     </div>
-    <div class="flex justify-center text-xs font-mono">
+    <div class="flex justify-center text-xs font-mono animate-fade animate-once animate-ease-in">
       &copy; {{ year }} CMRD Consulting LLC
     </div>
   </div>
